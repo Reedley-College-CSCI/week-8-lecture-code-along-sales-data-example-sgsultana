4e1a28a694c692b50ae90886620ea380a28e9229
Simon Sultana <ss063@reedleycollege.edu>
2026-03-10 09:45:07 -0700

    Declared array of doubles for sales and reported first value.

--- sales_data.cpp
+++ sales_data.cpp
@@ -3,6 +3,13 @@
 #include <iostream>
 using namespace std;
 int main() {
+        const int MAX_SIZE = 10;
+    int numDays = 7;
+
+    double amounts[MAX_SIZE] = { 1200, 1800, 900,
+    1500, 800, 1100, 1300 };
+
     cout << "Welcome to Store Sales Data Analysis!" << endl;
+    cout << "Day 1 sales: " << amounts[0] << endl;
     return 0;
 }
